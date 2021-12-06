76eca9ae9c2bb51079219264d84f5e54582695b4
Austin Ely <austiely@amazon.com>
2021-12-06 15:33:14 -0500

    Update beeminder.sh

--- scripts/beeminder.sh
+++ scripts/beeminder.sh
@@ -3,5 +3,5 @@ set -euo pipefail
 
 GAMES=$(curl https://lichess.org/api/user/$LICHESS_USERNAME | jq '.perfs.blitz.games + .perfs.rapid.games')
 curl \
-  --data "auth_token=${BEEMINDER_API_TOKEN}&value=${GAMES}&comment=sent by Copilot from AWS" \
+  --data "auth_token=${BEEMINDER_API_TOKEN}&value=${GAMES}&comment=${GAMES} played as of $(date +"%H:%M:%S")" \
   "https://www.beeminder.com/api/v1/users/${BEEMINDER_USERNAME}/goals/${GOAL}/datapoints.json"
